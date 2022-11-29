5d02c38b5e5dc34bf74d852046a646b4c5e44865
TornikeP <112972102+TornikeP@users.noreply.github.com>
2022-11-30 03:18:40 +0400

    add and remove Fav 46

--- src/script.js
+++ src/script.js
@@ -25,7 +25,7 @@ btn.addEventListener('click', add_searched_movies_to_dom)
 
 async function add_searched_movies_to_dom () {
     const data = await get_movie_by_search(input.value)
-    console.log(data)
+   
 
     main_grid_title.innerText = `Search Results...`
     main_grid.innerHTML = data.map(e => {
@@ -61,7 +61,7 @@ async function get_movie_by_id (id) {
     return respData
 }
 
-get_movie_trailer (414906)
+
 async function get_movie_trailer (id) {
     const resp = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`)
     const respData = await resp.json()
@@ -69,15 +69,15 @@ async function get_movie_trailer (id) {
    
 }
 
-
-
-
 async function show_popup (card) {
     popup_container.classList.add('show-popup')
 
     const movie_id = card.getAttribute('data-id')
     const movie = await get_movie_by_id(movie_id)
     const movie_trailer = await get_movie_trailer(movie_id)
+
+    // console.log(movie_trailer);
+
 
     popup_container.style.background = `linear-gradient(rgba(0, 0, 0, .8), rgba(0, 0, 0, 1)), url(${image_path + movie.poster_path})`
 
@@ -136,6 +136,20 @@ async function show_popup (card) {
                 </div>
             </div>
     `
+    const x_icon = document.querySelector('.x-icon')
+    x_icon.addEventListener('click', () => popup_container.classList.remove('show-popup'))
 
+
+    const heart_icon = popup_container.querySelector('.heart-icon')
+
+    heart_icon.addEventListener('click', () => {
+        if(heart_icon.classList.contains('change-color')) {
+            heart_icon.classList.remove('change-color')
+        } else {
+            heart_icon.classList.add('change-color')
+        }
+        fetch_favorite_movies()
+    })
 }
+
 
