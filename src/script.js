cf6a15e0bdc8987cbe9c43631bd0bc332a8a31d4
TornikeP <112972102+TornikeP@users.noreply.github.com>
2022-11-29 23:23:28 +0400

    HTML-ში popup-ის შექმნამდე

--- src/script.js
+++ src/script.js
@@ -8,6 +8,12 @@ const main_grid_title = document.querySelector('.favorites h1')
 const main_grid = document.querySelector('.movies-grid')
 
 
+function add_click_effect_to_card (cards) {
+    cards.forEach(card => {
+        card.addEventListener('click', () => show_popup(card))
+    })
+}
+
 async function get_movie_by_search (search_term) {
     const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search_term}`)
     const respData = await resp.json()
@@ -42,6 +48,12 @@ async function add_searched_movies_to_dom () {
         `
     }).join('')
 
+    const cards = document.querySelectorAll('.card')
+    add_click_effect_to_card(cards)
     
+}
+
+function show_popup (card) {
+    console.log('Popup is shown _ card');
 }
 
